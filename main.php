c2e60706cfad098ad2e97d4cdb7f2f830ce2e638
Mariel Castro <marielfx@gmail.com>
2024-02-01 21:07:38 -0600

    Inciando archivos

---
+++ main.php
@@ -0,0 +1,28 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <?php 
+        include_once("header.html");
+       ?>
+    </header>
+    
+    <main>
+
+    </main>
+
+    <footer>
+        <?php
+        include_once("footer.html");
+        ?>
+    </footer>
+
+</body>
+</html>
